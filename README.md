b769d4938ac00c05e3824a495fc22db7d6d5d1b5
Gavin Fynbo <gavfynbo@gmail.com>
2017-04-29 21:43:47 -0400

    added all pages

---
+++ README.md
@@ -0,0 +1,11 @@
+StepsAfter
+==========
+
+An application designed to help people who have, are currently, or fear dealing
+with an overdose to them or someone close to them.
+
+Contributers
+------------
+* Gavin Fynbo
+* Tatiana Diaz-Gallegos
+* Jio Jung
--- stepsafter.py
+++ stepsafter.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Index Page'
+def home():
+    return render_template('home.html')
+
+@app.route('/after-an-od')
+def after_an_od():
+    return render_template('after-an-od.html')
+
+@app.route('/facilities')
+def facilities():
+    return render_template('facilities.html')
 
 @app.route('/help')
 def hello():
